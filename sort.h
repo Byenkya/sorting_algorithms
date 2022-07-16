0c2f2a502e8bd0c5873ef2ba192a0287ba6dcd53
Byenkya <jbyenkya@idi.co.ug>
2022-07-16 14:09:03 +0300

    changes

--- sort.h
+++ sort.h
@@ -16,13 +16,11 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap_int(int *array, size_t a, size_t b);
 void swap_int1(int *array, int a, int b);
 void recursive_quick_sort(int *array, size_t size, int start, int end);
-size_t partition(int *array, size_t size, int start, int end);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
